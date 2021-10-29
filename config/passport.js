50d10342573217da2f5b11f56ff66e067b428270
Aavash Dhimal <aavashdhimal9@gmail.com>
2021-10-29 13:53:16 +0545

    bigrecha

---
+++ config/passport.js
@@ -0,0 +1,17 @@
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtraceJwt = require('passport-jwt').ExtractJwt;
+const mongoose = require('mongoose');
+const User = mongoose.model('users');
+const keys = require('../config/keys');
+
+const opts ={};
+opts.jwtFormRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = keys.secretOrKey;
+
+module.exports = passport =>{
+    passport.use(
+        new JwtStrategy(opts, (jwt_payload,done) => {
+            console.log(jwt_payload);
+        })
+    )
+}
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,83 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const passport = require('passport');
 
-router.get('/test', (req,res) => res.json({msg:"users work"}) );
 
-module.exports = router;
+
+const User = require("../../modles/User");
+const keys = require("../../config/keys");
+
+
+
+router.get("/test", (req, res) => res.json({ msg: "users work" }));
+
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then((user) => {
+    if (user) {
+      return res.status(400).json({ email: "Email already Exists" });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password,
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then((user) => res.json(user))
+            .catch((err) => console.log(err));
+        });
+      });
+    }
+  });
+});
+
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email }).then((user) => {
+    if (!user) {
+      return res.status(404).json({ email: "user not found" });
+    }
+    bcrypt.compare(password, user.password).then((isMatch) => {
+      if (isMatch) {
+        const payload = { id: user.id, name: user.name, avatar: user.avatar };
+
+        jwt.sign(
+           payload,
+           'xyz',
+           {expiresIn: 3600},
+           (err, token) => {
+             if(err){
+               res.json({msg:"error" + err})
+             }
+           res.json({
+            success: true,
+            token: "Bearer " + token,
+            });
+        });
+      } else {
+        return res.status(400).json({ password: "Incorrect" });
+      }
+    });
+  });
+});
+
+//current
+router.get('/current',passport.authenticate('jwt',{ session: false}),(req,res) => {
+  es.json({msg: 'success'});
+})
+module.exports = router; 
